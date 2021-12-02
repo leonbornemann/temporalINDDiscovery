71c0803f0c8e757072b719abaf652607ce33e7d8
leonbornemann <leonbornemann@gmx.de>
2021-12-02 15:57:32 +0100

    Improved Table Export output Structure

--- src/main/scala/de/hpi/temporal_ind/data/column/TableExportForManyMain.scala
+++ src/main/scala/de/hpi/temporal_ind/data/column/TableExportForManyMain.scala
@@ -6,21 +6,26 @@ import scala.util.Random
 
 object TableExportForManyMain extends App {
   val inputDir = args(0)
-  val outputDir = args(1)
+  val outputDirRootDir = args(1)
+
   val inputTimestamp = Instant.parse(args(2))
-  val randomNumberOfFilesToChoose = 2
-  val random = new Random(13)
-  val files = random.shuffle(new File(inputDir).listFiles().toIndexedSeq).take(10)
+  val outputDir = outputDirRootDir + s"/$inputTimestamp/"
+//  val randomNumberOfFilesToChoose = 10
+//  val random = new Random(13)
+//  val files = random.shuffle(new File(inputDir).listFiles().toIndexedSeq).take(randomNumberOfFilesToChoose)
+  val files = new File(inputDir).listFiles()
   files.foreach(f => {
     val vhs = ColumnHistory.fromJsonObjectPerLineFile(f.getAbsolutePath)
-    val byTable = vhs.zipWithIndex
+    val resultDir = new File(s"$outputDir/${f.getName}/")
+    resultDir.mkdir()
+    vhs.zipWithIndex
       .groupBy(t => (t._1.pageID,t._1.tableId))
-      .foreach{case ((pID,tID),colList) => {
+      .foreach{case ((pID,tID),colList) =>
         val headerMap = colList.map(t => (t._1,t._1.id)).toMap
         val colListSorted = colList.sortBy(_._2).toIndexedSeq
         val headersOrdered = colListSorted.map(t => headerMap(t._1))
-        ColumnVersion.serializeToTable(colListSorted.map(_._1.versionAt(inputTimestamp)),headersOrdered,new File(outputDir + s"/${tID}_$pID.csv"))
-      }}
+        ColumnVersion.serializeToTable(colListSorted.map(_._1.versionAt(inputTimestamp)),headersOrdered,new File(resultDir + s"/${tID}_$pID.csv"))
+      }
   })
 
 }
