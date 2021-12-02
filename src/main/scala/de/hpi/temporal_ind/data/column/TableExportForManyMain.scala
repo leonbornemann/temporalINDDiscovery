b0399edffa9da8a16f48b69b25f619524b1c2f2f
leonbornemann <leonbornemann@gmx.de>
2021-12-02 16:06:07 +0100

    Improved Table Export output Structure

--- src/main/scala/de/hpi/temporal_ind/data/column/TableExportForManyMain.scala
+++ src/main/scala/de/hpi/temporal_ind/data/column/TableExportForManyMain.scala
@@ -1,13 +1,15 @@
 package de.hpi.temporal_ind.data.column
 
+import de.hpi.temporal_ind.data.wikipedia.GLOBAL_CONFIG
+
 import java.io.File
-import java.time.{Instant, LocalDate}
+import java.time.temporal.ChronoUnit
+import java.time.{Duration, Instant, LocalDate}
 import scala.util.Random
 
 object TableExportForManyMain extends App {
   val inputDir = args(0)
   val outputDirRootDir = args(1)
-
   val inputTimestamp = Instant.parse(args(2))
   val outputDir = outputDirRootDir + s"/$inputTimestamp/"
 //  val randomNumberOfFilesToChoose = 10
@@ -15,9 +17,9 @@ object TableExportForManyMain extends App {
 //  val files = random.shuffle(new File(inputDir).listFiles().toIndexedSeq).take(randomNumberOfFilesToChoose)
   val files = new File(inputDir).listFiles()
   files.foreach(f => {
-    val vhs = ColumnHistory.fromJsonObjectPerLineFile(f.getAbsolutePath)
     val resultDir = new File(s"$outputDir/${f.getName}/")
-    resultDir.mkdir()
+    resultDir.mkdirs()
+    val vhs = ColumnHistory.fromJsonObjectPerLineFile(f.getAbsolutePath)
     vhs.zipWithIndex
       .groupBy(t => (t._1.pageID,t._1.tableId))
       .foreach{case ((pID,tID),colList) =>
