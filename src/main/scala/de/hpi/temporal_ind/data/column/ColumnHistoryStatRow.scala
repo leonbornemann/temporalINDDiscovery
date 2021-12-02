91c687231136fb894c353548d8891d98e5dbfc1c
leonbornemann <leonbornemann@gmx.de>
2021-12-02 16:30:17 +0100

    Added Filtering to TableExportForManyMain, improved stat row, so stats can be accessed

--- src/main/scala/de/hpi/temporal_ind/data/column/ColumnHistoryStatRow.scala
+++ src/main/scala/de/hpi/temporal_ind/data/column/ColumnHistoryStatRow.scala
@@ -7,21 +7,22 @@ import java.time.temporal.ChronoUnit
 
 case class ColumnHistoryStatRow(ch: ColumnHistory) {
 
+  val nVersions = ch.columnVersions.size
+  val valueChangeVersions = ch.versionsWithNonDeleteChanges
+  val durationInDays = ch.firstInsertToLastDeleteTimeInDays(GLOBAL_CONFIG.latestInstantWikipedia)
+  val nVersionsWithChanges = valueChangeVersions.size
+  val lifetimeInDays = ch.totalLifeTimeInDays(GLOBAL_CONFIG.latestInstantWikipedia)
+  val valueSets = valueChangeVersions.map(_.values)
+  val valueSetSizes = valueSets.map(_.size)
+  val sizeStatistics = ValueSequenceStatistics(valueSetSizes.map(_.toDouble))
+  val valueSetsWithIndex = valueSets
+    .zipWithIndex
+  val inverseJaccardSimilarities = valueSetsWithIndex
+    .withFilter(t => t._2!=0)
+    .map{case (values,i) => 1.0 - Util.Jaccard_Similarity(valueSetsWithIndex(i-1)._1,values)}
+  val changeAmountStatistics = ValueSequenceStatistics(inverseJaccardSimilarities)
+
   def toCSVLine = {
-    val nVersions = ch.columnVersions.size
-    val valueChangeVersions = ch.versionsWithNonDeleteChanges
-    val durationInDays = ch.firstInsertToLastDeleteTimeInDays(GLOBAL_CONFIG.latestInstantWikipedia)
-    val nVersionsWithChanges = valueChangeVersions.size
-    val lifetimeInDays = ch.totalLifeTimeInDays(GLOBAL_CONFIG.latestInstantWikipedia)
-    val valueSets = valueChangeVersions.map(_.values)
-    val valueSetSizes = valueSets.map(_.size)
-    val sizeStatistics = ValueSequenceStatistics(valueSetSizes.map(_.toDouble))
-    val valueSetsWithIndex = valueSets
-      .zipWithIndex
-    val inverseJaccardSimilarities = valueSetsWithIndex
-      .withFilter(t => t._2!=0)
-      .map{case (values,i) => 1.0 - Util.Jaccard_Similarity(valueSetsWithIndex(i-1)._1,values)}
-    val changeAmountStatistics = ValueSequenceStatistics(inverseJaccardSimilarities)
     val values = Seq(
       ch.id,
       ch.tableId,
--- src/main/scala/de/hpi/temporal_ind/data/column/TableExportForManyMain.scala
+++ src/main/scala/de/hpi/temporal_ind/data/column/TableExportForManyMain.scala
@@ -9,13 +9,15 @@ import java.time.{Duration, Instant, LocalDate}
 import scala.util.Random
 
 object TableExportForManyMain extends App with StrictLogging{
+  println(s"Called with ${args.toIndexedSeq}")
   val inputDir = args(0)
   val outputDirRootDir = args(1)
   val inputTimestamp = Instant.parse(args(2))
   val outputDir = outputDirRootDir + s"/$inputTimestamp/"
-//  val randomNumberOfFilesToChoose = 10
-//  val random = new Random(13)
-//  val files = random.shuffle(new File(inputDir).listFiles().toIndexedSeq).take(randomNumberOfFilesToChoose)
+  //FILTER CONFIG:
+  val minMedianSize = 5
+  val minLifetimeDays = 30
+  //END FILTER CONFIG
   val files = new File(inputDir).listFiles()
   files
     .zipWithIndex
@@ -25,6 +27,10 @@ object TableExportForManyMain extends App with StrictLogging{
       resultDir.mkdirs()
       val vhs = ColumnHistory.fromJsonObjectPerLineFile(f.getAbsolutePath)
       vhs.zipWithIndex
+        .filter(vhs => {
+          val stats = ColumnHistoryStatRow(vhs._1)
+          stats.sizeStatistics.median>=minMedianSize && stats.lifetimeInDays >= minLifetimeDays
+        })
         .groupBy(t => (t._1.pageID,t._1.tableId))
         .foreach{case ((pID,tID),colList) =>
           val headerMap = colList.map(t => (t._1,t._1.id)).toMap
