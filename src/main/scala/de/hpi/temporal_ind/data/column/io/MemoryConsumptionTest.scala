64250490c9c2bf30b046acd8b72a77438f7c4670
leonbornemann <leonbornemann@gmx.de>
2022-01-03 17:44:05 +0100

    Fixed MemoryConsumptionTest

--- src/main/scala/de/hpi/temporal_ind/data/column/io/MemoryConsumptionTest.scala
+++ src/main/scala/de/hpi/temporal_ind/data/column/io/MemoryConsumptionTest.scala
@@ -8,9 +8,10 @@ import scala.io.Source
 object MemoryConsumptionTest extends App {
 
   val inputDir = args(0)
-  val listOfAllColumnHistories = new File(inputDir).listFiles().map(f =>
-    ColumnHistory.fromJsonFile(f.getAbsolutePath)
-  )
+  val listOfAllColumnHistories = new File(inputDir).listFiles().flatMap { f =>
+    println(s"Reading ${f.getName}")
+    ColumnHistory.fromJsonObjectPerLineFile(f.getAbsolutePath)
+  }
   println(s"Size: ${listOfAllColumnHistories.size}")
   println("Enter anything to terminate")
   val res = io.StdIn.readLine()
