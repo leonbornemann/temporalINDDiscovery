d4589610e14843a7ca62ba4510ba71b1b62ab7cd
leonbornemann <leonbornemann@gmx.de>
2022-01-04 11:03:40 +0100

    Added collision count to MemoryConsumptionTest

--- src/main/scala/de/hpi/temporal_ind/data/column/io/MemoryConsumptionTest.scala
+++ src/main/scala/de/hpi/temporal_ind/data/column/io/MemoryConsumptionTest.scala
@@ -9,12 +9,18 @@ object MemoryConsumptionTest extends App {
 
   val inputDir = args(0)
   val listOfAllColumnHistories = collection.mutable.TreeMap[String,ColumnHistoryEncoded]()
+  var collisions = 0
   new File(inputDir).listFiles().foreach { f =>
     println(s"Reading ${f.getName}")
     val histories = ColumnHistoryEncoded.fromJsonObjectPerLineFile(f.getAbsolutePath)
-    histories.foreach(h => listOfAllColumnHistories.put(h.id,h))
+    histories.foreach(h => {
+      if(listOfAllColumnHistories.contains(h.id)){
+        collisions +=1
+      }
+      listOfAllColumnHistories.put(h.id,h)
+    })
   }
-  println(s"Size: ${listOfAllColumnHistories.size}")
+  println(s"Size: ${listOfAllColumnHistories.size} - num Collisions: $collisions")
   println("Enter anything to terminate")
   val res = io.StdIn.readLine()
   println(s"Entered $res")
