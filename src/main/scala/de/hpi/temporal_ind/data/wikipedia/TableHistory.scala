c304e1fabc37eadb823564c948e9b7eb8180dc56
leonbornemann <leonbornemann@gmx.de>
2022-08-10 11:56:15 +0200

    Hopefully fixed column duplicate issues

--- src/main/scala/de/hpi/temporal_ind/data/wikipedia/TableHistory.scala
+++ src/main/scala/de/hpi/temporal_ind/data/wikipedia/TableHistory.scala
@@ -20,18 +20,23 @@ case class TableHistory(pageID:String,
   def extractColumnHistories = {
     val colIDToColHistory = mutable.HashMap[String, ArrayBuffer[ColumnVersion]]()
     tables.foreach(tv => {
-      val expandedRatioHeader = tv.cells(0).filter(_.wasExpanded).size / tv.cells(0).size.toDouble
-      val expandedRatioMiddle = tv.cells(tv.cells.size / 2).filter(_.wasExpanded).size / tv.cells(0).size.toDouble
-      val expandedRatioEnd = tv.cells(tv.cells.size / 2).filter(_.wasExpanded).size / tv.cells(0).size.toDouble
-      val filterExpanded = expandedRatioHeader>0.5 && expandedRatioMiddle>0.5 && expandedRatioEnd > 0.5
-      val tableState = tv.getTableState(filterExpanded)
-      tableState.artificialHeaders
-        .zip(tableState.columns)
-        .foreach { case (id, version) =>
-          val curHistory = colIDToColHistory.getOrElseUpdate(id, ArrayBuffer[ColumnVersion]())
-          if (curHistory.isEmpty || curHistory.last.values != version.values)
-            curHistory.append(version)
-        }
+      val tableState = if(tv.cells.size>0){
+        val expandedRatioHeader = tv.cells(0).filter(_.wasExpanded).size / tv.cells(0).size.toDouble
+        val expandedRatioMiddle = tv.cells(tv.cells.size / 2).filter(_.wasExpanded).size / tv.cells(0).size.toDouble
+        val expandedRatioEnd = tv.cells(tv.cells.size / 2).filter(_.wasExpanded).size / tv.cells(0).size.toDouble
+        val filterExpanded = expandedRatioHeader>0.5 && expandedRatioMiddle>0.5 && expandedRatioEnd > 0.5
+        val tableState = tv.getTableState(filterExpanded)
+        tableState.artificialHeaders
+          .zip(tableState.columns)
+          .foreach { case (id, version) =>
+            val curHistory = colIDToColHistory.getOrElseUpdate(id, ArrayBuffer[ColumnVersion]())
+            if (curHistory.isEmpty || curHistory.last.values != version.values)
+              curHistory.append(version)
+          }
+        tableState
+      } else {
+        TableState(IndexedSeq(),IndexedSeq())
+      }
       //process non-present columns in this version
       val curColumnIds = tableState.artificialHeaders.toSet
       colIDToColHistory.keySet.diff(curColumnIds).foreach(id => {
