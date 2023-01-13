294a58b8274d755dc2021b7ba8a12ed817a268c3
leonbornemann <leonbornemann@gmx.de>
2023-01-13 17:55:13 +0100

    updated stats row

--- src/main/scala/de/hpi/temporal_ind/data/column/statistics/StatisticsExportMain.scala
+++ src/main/scala/de/hpi/temporal_ind/data/column/statistics/StatisticsExportMain.scala
@@ -2,10 +2,12 @@ package de.hpi.temporal_ind.data.column.statistics
 
 import de.hpi.temporal_ind.data.column.data.original.ColumnHistory
 import de.hpi.temporal_ind.data.column.statistics
+import de.hpi.temporal_ind.data.wikipedia.GLOBAL_CONFIG
 
 import java.io.{File, PrintWriter}
 
 object StatisticsExportMain extends App {
+  GLOBAL_CONFIG.setSettingsForDataSource("wikipedia")
   val inputDir = new File(args(0))
   val resultFile = new PrintWriter(args(1))
   val iterable = ColumnHistory.iterableFromJsonObjectPerLineDir(inputDir, true)
