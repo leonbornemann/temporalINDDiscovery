cdbf42513bcc82052b8e04f4e27588449561613d
leonbornemann <leonbornemann@gmx.de>
2023-01-23 14:42:15 +0100

    Added BucketingMain

--- src/main/scala/de/hpi/temporal_ind/data/column/labelling/BucketingMain.scala
+++ src/main/scala/de/hpi/temporal_ind/data/column/labelling/BucketingMain.scala
@@ -1,5 +1,9 @@
 package de.hpi.temporal_ind.data.column.labelling
 
+import de.hpi.temporal_ind.data
+import de.hpi.temporal_ind.data.column
+import de.hpi.temporal_ind.data.column.data
+import de.hpi.temporal_ind.data.column.data.original
 import de.hpi.temporal_ind.data.column.data.original.ColumnHistory
 import de.hpi.temporal_ind.data.column.statistics.ColumnHistoryStatRow
 import de.hpi.temporal_ind.data.wikipedia.GLOBAL_CONFIG
@@ -15,7 +19,6 @@ object BucketingMain extends App {
     .listFiles()
     .map(f => (f,ColumnHistory.iterableFromJsonObjectPerLineFile(f.getAbsolutePath,true)))
   val buckets = Seq((1,2),(2,4),(4,8),(8,16),(16,32),(32,20000))
-  val curFiles = collection.mutable.HashMap[String,PrintWriter]()
   val bucketSizes = collection.mutable.HashMap[String,Int]()
   def getBucket(nVersionsWithChanges: Int, buckets: Seq[(Int, Int)]) =
     buckets
@@ -27,25 +30,36 @@ object BucketingMain extends App {
     s"${min}_$max"
   }
 
-  def appendToCurrentFile(ch: ColumnHistory, bucketName: String,originFile:File) = {
+  def appendToCurrentFile(curFiles:collection.mutable.HashMap[String,PrintWriter],ch: ColumnHistory, bucketName: String,originFile:File) = {
     val newDir = new File(outputDir + "/" + bucketName)
     if(!newDir.exists()){
       println(s"Creating new Dir $newDir")
     }
     newDir.mkdirs()
-    val filePath = outputDir + "/" + bucketName + "/" + originFile
+    val filePath = outputDir + "/" + bucketName + "/" + originFile.getName
     val writer = curFiles.getOrElseUpdate(filePath, new PrintWriter(filePath))
     bucketSizes.put(bucketName,bucketSizes.getOrElse(bucketName,0))
     ch.appendToWriter(writer)
   }
 
+  def process(f: File, chs: ColumnHistory.JsonObjectPerLineFileIterator) = {
+    val curFiles = collection.mutable.HashMap[String,PrintWriter]()
+    chs
+      .map { case (ch) => {
+        (ch, ColumnHistoryStatRow(ch), f)
+      }}
+      .withFilter { case (_, stats, _) => {
+        stats.sizeStatistics.median >= 2 && stats.lifetimeInDays >= 30 && stats.nVersionsWithChanges > 0
+      }
+      }
+      .map { case (ch, stats, f) => (ch, getDirName(getBucket(stats.nVersionsWithChanges, buckets)), f) }
+      .foreach { case (ch, dirName, originFile) => appendToCurrentFile(curFiles,ch, dirName, originFile) }
+    curFiles.values.foreach(_.close())
+  }
+
   iterators
-    .flatMap{case (f,chs) => chs.map(ch => (ch,f))}
-    .map{case (ch,f) => (ch,ColumnHistoryStatRow(ch),f)}
-    .withFilter{case (_,stats,_) => stats.sizeStatistics.median >= 2 && stats.lifetimeInDays >= 30 && stats.nVersionsWithChanges>0}
-    .map { case (ch,stats,f) => (ch,getDirName(getBucket(stats.nVersionsWithChanges,buckets)),f)}
-    .foreach{ case (ch,dirName,originFile) => appendToCurrentFile(ch,dirName,originFile)}
-  curFiles.values.foreach(_.close())
+    .foreach{case (f,chs) => process(f,chs)}
+
   bucketSizes
     .toIndexedSeq
     .sortBy(_._1)
