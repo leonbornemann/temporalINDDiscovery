9df1ed66ce6f9d3ba1c923f04ee18185d3721a95
leonbornemann <leonbornemann@gmx.de>
2023-01-23 17:27:54 +0100

    Modified ExportForManyMain so that it works for buckets

--- src/main/executables/runMANYExportANDMANY.sh
+++ src/main/executables/runMANYExportANDMANY.sh
@@ -1,16 +1,16 @@
 bash pullAndCompileTemporalINDCode.sh
-timestamp="2016-11-05T03:57:33Z;2017-11-05T03:57:33Z"
-manyExportRootDir="/san2/data/change-exploration/temporalIND/exportedForMANY/wikipedia/"
-rm "$manyExportRootDir/$timestamp/" -r
+timestamp="2017-11-05T03:57:33Z"
+manyExportRootDir="/san2/data/change-exploration/temporalIND/exportedForMANY/wikipedia/bucketed/"
 echo "Running ExportForManyMain"
-filteredOutputPath="/san2/data/change-exploration/temporalIND/columnHistories/wikipediaFilteredNewUnion/"
-mkdir $filteredOutputPath
-java -ea -Xmx32g -cp code/temporalINDDiscovery/target/scala-2.13/INDInTemporalData-assembly-0.1.jar de.hpi.temporal_ind.data.column.ExportForManyMain wikipedia /san2/data/change-exploration/temporalIND/columnHistories/wikipediaNew/ $filteredOutputPath $manyExportRootDir $timestamp
+java -ea -Xmx32g -cp code/temporalINDDiscovery/target/scala-2.13/INDInTemporalData-assembly-0.1.jar de.hpi.temporal_ind.data.column.ExportForManyMain wikipedia /san2/data/change-exploration/temporalIND/columnHistories/buckets_filtered/ $manyExportRootDir $timestamp
 echo "Finished ExportForManyMain - Begin Running MANY"
-basePathInput="/san2/data/change-exploration/temporalIND/exportedForMANY/wikipedia/"
-basePathResults="/san2/data/change-exploration/temporalIND/discoveredByMANY/wikipedia/"
+#basePathInput="/san2/data/change-exploration/temporalIND/exportedForMANY/wikipedia/"
+#basePathResults="/san2/data/change-exploration/temporalIND/discoveredByMANY/wikipedia/"
 #snapshotTimes=( "2004-07-26T12:55:44Z" "2007-11-19T12:55:44Z" "2011-07-26T12:55:44Z"  "2015-07-26T12:55:44Z")
-currentInstant=$timestamp
-echo "Running $currentInstant"
-java -ea -Xmx190g -cp jars/MANY-1.2-SNAPSHOT.jar de.metanome.algorithms.many.driver.AnelosimusDriver -inputFileEnding .csv -inputFolderPath "$basePathInput/$currentInstant/" -filterNonUniqueRefs false -outputFile "$basePathResults/${currentInstant}_allowNonUniquePK.txt" -hasHeader true -filterNumericAndShortCols true -filterNullCols true -dop 25  > logs/manyExecutionLogs/${currentInstant}.log 2>&1
-shuf $basePathResults/${currentInstant}.txt -o $basePathResults/${currentInstant}.txt
+#currentInstant="2016-11-05T03:57:33Z_Some(2017-11-05T03:57:33Z)"
+#echo "Running $currentInstant"
+#echo $basePathResults
+#echo $basePathInput
+#echo $currentInstant
+#java -ea -Xmx190g -cp jars/MANY-1.2-SNAPSHOT.jar de.metanome.algorithms.many.driver.AnelosimusDriver -inputFileEnding .csv -inputFolderPath "$basePathInput/$currentInstant/" -filterNonUniqueRefs false -outputFile "$basePathResults/${currentInstant}_allowNonUniquePK.txt" -hasHeader true -filterNumericAndShortCols true -filterNullCols true -dop 25  > logs/manyExecutionLogs/${currentInstant}.log 2>&1
+#shuf $basePathResults/${currentInstant}.txt -o $basePathResults/${currentInstant}.txt
--- src/main/scala/de/hpi/temporal_ind/data/column/ExportForManyMain.scala
+++ src/main/scala/de/hpi/temporal_ind/data/column/ExportForManyMain.scala
@@ -11,64 +11,56 @@ import java.time.Instant
 object ExportForManyMain extends App with StrictLogging{
   println(s"Called with ${args.toIndexedSeq}")
   GLOBAL_CONFIG.setSettingsForDataSource(args(0))
-  val inputDir = args(1)
-  val outputRootDirLineages = args(2)
-  val outputDirRootDir = args(3)
-  val (beginTimestamp,endTimestampExclusive) = if(args(4).contains(";"))
-    (Instant.parse(args(4).split(";")(0)),Some(Instant.parse(args(4).split(";")(1))))
+  val inputRootDir = new File(args(1))
+  val outputDirRootDir = args(2)
+  val (beginTimestamp,endTimestampExclusive) = if(args(3).contains(";"))
+    (Instant.parse(args(3).split(";")(0)),Some(Instant.parse(args(3).split(";")(1))))
   else {
-    (Instant.parse(args(4)),None)
+    (Instant.parse(args(3)),None)
   }
-  val outputDir = outputDirRootDir + s"/${beginTimestamp}_$endTimestampExclusive/"
-  val versonRangeEndExlcusive = endTimestampExclusive.getOrElse(beginTimestamp.plusNanos(1))
-  //FILTER CONFIG:
-  val minMedianSize = 5
-  val minLifetimeDays = 30
-  val minNonDeleteChanges = 10
-  //END FILTER CONFIG
-  val files = new File(inputDir).listFiles()
-  val preparer = new WikipediaDataPreparer()
-  files
-    .zipWithIndex
-    .foreach{ case (f,i) =>
-      logger.debug(s"Processing ${f.getAbsolutePath} ($i/${files.size}")
-      val resultDir = new File(s"$outputDir/${f.getName}/")
-      resultDir.mkdirs()
-      val resultWriterFilteredHistory = new PrintWriter(s"$outputRootDirLineages/${f.getName}.json")
-      val vhs = ColumnHistory.fromJsonObjectPerLineFile(f.getAbsolutePath)
-      val filtered = vhs.zipWithIndex
-        .filter(vhs => {
-          val stats = ColumnHistoryStatRow(vhs._1)
-          val nonDelete:Boolean = if(endTimestampExclusive.isEmpty)
-            !vhs._1.versionAt(beginTimestamp).isDelete
-          else
-            vhs._1.existsNonDeleteInVersionRange(beginTimestamp,endTimestampExclusive.get)
-          nonDelete &&
-            stats.sizeStatistics.median>=minMedianSize &&
-            stats.lifetimeInDays >= minLifetimeDays &&
-            vhs._1.versionsWithNonDeleteChanges.size >= minNonDeleteChanges &&
-            !preparer.mostlyNumeric(vhs._1) &&
-            !preparer.isNumeric(vhs._1.columnVersions.findLast(!_.isDelete).get.values)
-        })
-      //serialize to filtered outputDir:
-      filtered.foreach{case (ch,_) => ch.appendToWriter(resultWriterFilteredHistory)}
-      resultWriterFilteredHistory.close()
-      filtered
-        .groupBy(t => (t._1.pageID,t._1.tableId))
-        .foreach{case ((pID,tID),colList) =>
-          val headerMap = colList.map(t => (t._1,t._1.id)).toMap
-          val colListSorted = colList.sortBy(_._2).toIndexedSeq
-          val headersOrdered = if(!endTimestampExclusive.isDefined)
-            colListSorted.map(t => headerMap(t._1))
-          else
-            colListSorted.flatMap(t => Seq(headerMap(t._1) + "_union",headerMap(t._1) ))
-          val tableVersion = if(!endTimestampExclusive.isDefined)
-            colListSorted.map(_._1.versionAt(beginTimestamp))
-          else
-            colListSorted.flatMap(t => Seq(t._1.versionUnion(beginTimestamp,endTimestampExclusive.get),t._1.versionAt(beginTimestamp)))
-          if(tableVersion.exists(_.values.size!=0))
-            ColumnVersion.serializeToTable(tableVersion,headersOrdered,new File(resultDir + s"/${tID}_$pID.csv"))
-        }
-  }
+  inputRootDir.listFiles().foreach(inputDir => {
+    println(s"Processing $inputDir")
+    val outputDir = outputDirRootDir + s"/${inputDir.getName}/"
+    val files = inputDir.listFiles()
+    val preparer = new WikipediaDataPreparer()
+    files
+      .zipWithIndex
+      .foreach { case (f, i) =>
+        logger.debug(s"Processing ${f.getAbsolutePath} ($i/${files.size}")
+        val resultDir = new File(s"$outputDir/${f.getName}/")
+        resultDir.mkdirs()
+        val vhs = ColumnHistory.fromJsonObjectPerLineFile(f.getAbsolutePath)
+        val filtered = vhs.zipWithIndex
+          .filter(vhs => {
+            val nonDelete: Boolean = if (endTimestampExclusive.isEmpty)
+              !vhs._1.versionAt(beginTimestamp).isDelete
+            else
+              vhs._1.existsNonDeleteInVersionRange(beginTimestamp, endTimestampExclusive.get)
+            nonDelete &&
+              !preparer.mostlyNumeric(vhs._1) &&
+              !preparer.isNumeric(vhs._1.columnVersions.findLast(!_.isDelete).get.values)
+          })
+        //serialize to filtered outputDir:
+        filtered.foreach { case (ch, _) => ch.appendToWriter(resultWriterFilteredHistory) }
+        resultWriterFilteredHistory.close()
+        filtered
+          .groupBy(t => (t._1.pageID, t._1.tableId))
+          .foreach { case ((pID, tID), colList) =>
+            val headerMap = colList.map(t => (t._1, t._1.id)).toMap
+            val colListSorted = colList.sortBy(_._2).toIndexedSeq
+            val headersOrdered = if (!endTimestampExclusive.isDefined)
+              colListSorted.map(t => headerMap(t._1))
+            else
+              colListSorted.flatMap(t => Seq(headerMap(t._1) + "_union", headerMap(t._1)))
+            val tableVersion = if (!endTimestampExclusive.isDefined)
+              colListSorted.map(_._1.versionAt(beginTimestamp))
+            else
+              colListSorted.flatMap(t => Seq(t._1.versionUnion(beginTimestamp, endTimestampExclusive.get), t._1.versionAt(beginTimestamp)))
+            if (tableVersion.exists(_.values.size != 0))
+              ColumnVersion.serializeToTable(tableVersion, headersOrdered, new File(resultDir + s"/${tID}_$pID.csv"))
+          }
+      }
+  })
+
 
 }
