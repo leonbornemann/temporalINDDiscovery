61e29fc4119a85d660f53bcc7756a840e3c57eb8
leonbornemann <leonbornemann@gmx.de>
2023-02-02 06:46:28 +0100

    Updated LabelledExampleExport

--- src/main/scala/de/hpi/temporal_ind/data/column/labelling/TINDCandidateExportForLabelling.scala
+++ src/main/scala/de/hpi/temporal_ind/data/column/labelling/TINDCandidateExportForLabelling.scala
@@ -10,22 +10,26 @@ import java.io.{File, PrintWriter}
 object TINDCandidateExportForLabelling extends App {
   println(s"called with ${args.toIndexedSeq}")
   GLOBAL_CONFIG.setSettingsForDataSource(args(0))
-  val inputFile = args(1)
+  val inputDir = args(1)
   val columnHistoryDir = args(2)
-  val outputFileToLabel = args(3)
+  val outputDirToLabel = args(3)
   val filterByUnion = args(4).toBoolean
   //val outputFileStatistics = args(4)
   val version = GLOBAL_CONFIG.lastInstant
   val sampleSize = 2000
-  val pr = new PrintWriter(outputFileToLabel)
-  pr.println(INDCandidate.csvSchema)
   val indexed = IndexedColumnHistories.fromColumnHistoryJsonPerLineDir(columnHistoryDir)
-  InclusionDependencyFromMany.readFromMANYOutputFile(new File(inputFile))
-    .withFilter(tind => !filterByUnion || (tind.rhsColumnID.contains("union") && !tind.lhsColumnID.contains("union") && tind.rhsColumnID.replace("_union","")!=tind.lhsColumnID))
-    .take(sampleSize)
-    .map(ind => ind.toCandidate(indexed,filterByUnion))
-    .foreach(candidate => {
-      pr.println(candidate.toLabelCSVString(version))
-    })
-  pr.close()
+  new File(inputDir).listFiles().foreach(inputFile => {
+    val outputFileToLabel = outputDirToLabel + s"/${inputFile.getName}.json"
+    val pr = new PrintWriter(outputFileToLabel)
+    pr.println(INDCandidate.csvSchema)
+    InclusionDependencyFromMany.readFromMANYOutputFile(inputFile)
+      .withFilter(tind => !filterByUnion || (tind.rhsColumnID.contains("union") && !tind.lhsColumnID.contains("union") && tind.rhsColumnID.replace("_union", "") != tind.lhsColumnID))
+      .take(sampleSize)
+      .map(ind => ind.toCandidate(indexed, filterByUnion))
+      .foreach(candidate => {
+        pr.println(candidate.toLabelCSVString(version))
+      })
+    pr.close()
+  })
+
 }
