fea9df8748e104b7db53d0463b65c73b2b002c66
leonbornemann <leonbornemann@gmx.de>
2023-02-06 16:50:31 +0100

    Fixed StatisticsForLabelledExampleMain

--- src/main/scala/de/hpi/temporal_ind/data/column/labelling/StatisticsForLabelledExampleMain.scala
+++ src/main/scala/de/hpi/temporal_ind/data/column/labelling/StatisticsForLabelledExampleMain.scala
@@ -10,17 +10,22 @@ import scala.io.Source
 
 object StatisticsForLabelledExampleMain extends App {
   GLOBAL_CONFIG.setSettingsForDataSource(args(0))
-  val sourceFile = args(1)
-  val index =  new IncrementalIndexedColumnHistories(new File(args(2)))
-  val out = new PrintWriter(args(3))
+  val sourceDir = new File(args(1))
+  val index = new IncrementalIndexedColumnHistories(new File(args(2)))
+  private val resultDir: String = args(3)
+  new File(resultDir).mkdirs()
 
-  LabelledINDCandidateStatistics.printCSVSchema(out)
-  val examples = Source.fromFile(sourceFile)
-    .getLines()
-    .toIndexedSeq
-    .tail
-    .map(l => LabelledINDCandidateStatistics.fromCSVLineWithIncrementalIndex(index,l))
-    .foreach(ind => ind.serializeValidityStatistics(out))
-  out.close()
+  sourceDir.listFiles().foreach(sourceFile => {
+    val out = new PrintWriter(resultDir + "/stats_" + sourceFile.getName)
+    LabelledINDCandidateStatistics.printCSVSchema(out)
+    val examples = Source.fromFile(sourceFile)
+      .getLines()
+      .toIndexedSeq
+      .tail
+      .map(l => LabelledINDCandidateStatistics.fromCSVLineWithIncrementalIndex(index, l))
+      .foreach(ind => ind.serializeValidityStatistics(out))
+    out.close()
+  })
+
 
 }
