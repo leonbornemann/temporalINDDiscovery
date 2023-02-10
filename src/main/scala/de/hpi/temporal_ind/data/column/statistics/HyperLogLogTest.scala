b9bb6b6ebbbcaefdb7b75de3063148e5e9a4bd55
leonbornemann <leonbornemann@gmx.de>
2023-02-10 08:18:57 +0100

    Added HyperLogLogTest

--- src/main/scala/de/hpi/temporal_ind/data/column/statistics/HyperLogLogTest.scala
+++ src/main/scala/de/hpi/temporal_ind/data/column/statistics/HyperLogLogTest.scala
@@ -13,13 +13,15 @@ object HyperLogLogTest extends App {
   val inputDir = args(0)
   val outputFile = new PrintWriter(args(1))
   val index = new IncrementalIndexedColumnHistories(new File(inputDir))
-  outputFile.println("estimatedOverlap,trueOverlap")
+  outputFile.println("estimatedOverlap,trueOverlap,lhs,rhs")
   (0 until 1000).foreach(i => {
+    println(s"Processing ${i}")
     val a = index.getRandomColumnHistory()
     val b = index.getRandomColumnHistory()
     val estimatedOverlap = a.hyperLogLogOverlapOfUnionedValuesets(b)
     val trueOverlap = a.overlapOfUnionedValuesets(b)
-    outputFile.println(s"$estimatedOverlap,$trueOverlap")
+    outputFile.println(s"$estimatedOverlap,$trueOverlap,${a.toJson()},${b.toJson()}")
+    outputFile.flush()
   })
   outputFile.close()
 
