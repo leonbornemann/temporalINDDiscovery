d22a98471404ed3c2497b11ffc450d90d2bc6bed
leonbornemann <leonbornemann@gmx.de>
2023-04-25 10:10:39 +0200

    Changed sample query generation to generate 10 sets of queries

--- src/main/scala/de/hpi/temporal_ind/discovery/SampleQueryGeneration.scala
+++ src/main/scala/de/hpi/temporal_ind/discovery/SampleQueryGeneration.scala
@@ -8,11 +8,14 @@ import scala.util.Random
 object SampleQueryGeneration extends App{
 
   val random = new Random(13)
-  val target = new PrintWriter("sampleQueries.jsonl")
   val sourceFileBinary = args(0)
   val dataLoader = new InputDataManager(sourceFileBinary, None)
   val data = dataLoader.loadData()
-  val sample = random.shuffle(data).take(10000).map(och => och.columnHistoryID).foreach(s => s.appendToWriter(target))
-  target.close()
+  for(i <- (0 until 10)){
+    val target = new PrintWriter(s"sampleQueries_$i.jsonl")
+    val shuffled = random.shuffle(data)
+    val sample = shuffled.take(10000).map(och => och.columnHistoryID).foreach(s => s.appendToWriter(target))
+    target.close()
+  }
 
 }
