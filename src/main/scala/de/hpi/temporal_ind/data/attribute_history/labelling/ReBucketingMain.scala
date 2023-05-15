6d4cde42c113050ebc3eecd2e5aedc73bbd6afea
leonbornemann <leonbornemann@gmx.de>
2023-05-15 14:03:00 +0200

    Changed Bucketing

---
+++ src/main/scala/de/hpi/temporal_ind/data/attribute_history/labelling/ReBucketingMain.scala
@@ -0,0 +1,53 @@
+package de.hpi.temporal_ind.data.attribute_history.labelling
+
+import de.hpi.temporal_ind.data.attribute_history.data.metadata.ColumnHistoryMetadata
+
+import java.io.{File, PrintWriter}
+import scala.io.Source
+
+object ReBucketingMain extends App {
+  val labelledDir = new File(args(0))
+  val outputDir = new File(args(1))
+
+  val buckets = Seq((4, 8), (8, 16), (16, 20000))
+
+  implicit class Crossable[X](xs: Iterable[X]) {
+    def cross[Y](ys: Iterable[Y]) = for {x <- xs; y <- ys} yield (x, y)
+  }
+
+  val indBuckets = buckets.cross(buckets)
+  val bucketFiles = indBuckets.map { case b =>
+    val ((lmin, lmax), (rmin, rmax)) = b
+    val filename = s"$lmin-${lmax}__$rmin-$rmax.txt"
+    (b, new PrintWriter(s"$outputDir/$filename"))
+  }.toMap
+  val metadata = ColumnHistoryMetadata.readAsMap(args(2))
+  labelledDir.listFiles().foreach(f => {
+    val linesWithContent = Source.fromFile(f)
+      .getLines()
+      .toIndexedSeq
+      .tail
+    linesWithContent.foreach(l => {
+      val leftID = l.split(",")(13)
+      val rightID = l.split(",")(14)
+      val (leftBucket, rightBucket) = getBuckets(metadata(leftID).nChangeVersions, metadata(rightID).nChangeVersions, buckets)
+      bucketFiles((leftBucket, rightBucket)).println(l)
+    })
+  })
+  bucketFiles.foreach(_._2.close())
+
+
+
+
+  def getBuckets(nVersionsWithChangesLeft: Int, nVersionsWithChangesRight: Int, buckets: Seq[(Int, Int)]) = {
+    val leftBucket = buckets
+      .find { case (min, max) => nVersionsWithChangesLeft >= min && nVersionsWithChangesLeft <= max }
+      .get
+    val rightBucket = buckets
+      .find { case (min, max) => nVersionsWithChangesRight >= min && nVersionsWithChangesRight <= max }
+      .get
+    (leftBucket, rightBucket)
+  }
+
+
+}
