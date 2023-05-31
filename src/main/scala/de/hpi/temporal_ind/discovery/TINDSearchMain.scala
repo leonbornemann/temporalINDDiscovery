8fcd19ba5f7412322925e1b1004a29551c505932
leonbornemann <leonbornemann@gmx.de>
2023-05-31 23:16:25 +0200

    Added epsilon / delta variation

--- src/main/scala/de/hpi/temporal_ind/discovery/TINDSearchMain.scala
+++ src/main/scala/de/hpi/temporal_ind/discovery/TINDSearchMain.scala
@@ -12,14 +12,17 @@ import de.hpi.temporal_ind.util.TimeUtil
 import java.io.File
 
 object TINDSearchMain extends App with StrictLogging{
+
+  //0.00066 0,775743
+  (0 until 20).map(Math.pow(2,_)*0.00066).foreach(println(_))
   println(s"Called with ${args.toIndexedSeq}")
   GLOBAL_CONFIG.setSettingsForDataSource("wikipedia")
   println(GLOBAL_CONFIG.totalTimeInDays)
   val queryFiles = args(0).split(",").toIndexedSeq
   val targetRootDir = args(1)
   val sourceFileBinary = args(2)
-  val relativeEpsilon = args(3).toDouble
-  val maxDeltaWhileIndexing = args(4).toLong
+  val relativeEpsilons = args(3).split(",").map(_.toDouble)
+  val maxDeltasWhileIndexing = args(4).split(",").map(_.toLong)
   val timeSliceChoiceMethod = TimeSliceChoiceMethod.withName(args(5))
   val bloomFilterSizes = args(6).split(",").map(_.toInt).toIndexedSeq
   val seeds = args(7).split(",").map(_.toLong).toIndexedSeq
@@ -30,10 +33,7 @@ object TINDSearchMain extends App with StrictLogging{
   val indexDeltaFactors = args(12).split(",").map(_.toInt).toIndexedSeq
   val inputSizeFactors = args(13).split(",").map(_.toDouble).toIndexedSeq
   metaDataDir.mkdirs()
-  val absoluteExpectedEpsilon = relativeEpsilon * GLOBAL_CONFIG.totalTimeInNanos
-  val expectedOmega = new ConstantWeightFunction()
-  val maxDeltaInNanos = TimeUtil.nanosPerDay*maxDeltaWhileIndexing
-  val queryParameters = TINDParameters(absoluteExpectedEpsilon, maxDeltaInNanos, expectedOmega)
+
   val version = "0.99"
   val targetDir = new File(targetRootDir + s"/$version/")
   targetDir.mkdir()
@@ -52,36 +52,43 @@ object TINDSearchMain extends App with StrictLogging{
     logger.debug(s"Processing bloomFilterSize $bloomFilterSize")
     for (seed <- seeds) {
       logger.debug(s"Processing seed $seed")
-      indexEpsilonFactors.foreach(epsilonFactor => {
-        logger.debug(s"Processing epsilonFactor $epsilonFactor")
-        indexDeltaFactors.foreach(deltaFactor => {
-          logger.debug(s"Processing deltaFactor $deltaFactor")
-          val indexDelta = maxDeltaInNanos * deltaFactor
-          val indexEpsilon = absoluteExpectedEpsilon * epsilonFactor
-          val indexParameter = TINDParameters(indexEpsilon, indexDelta, expectedOmega)
-          for(inputSizeFactor <- inputSizeFactors){
-            logger.debug(s" Running inputSizeFactor $inputSizeFactor")
-            relaxedShiftedTemporalINDDiscovery.useSubsetOfData(inputSizeFactor)
-            relaxedShiftedTemporalINDDiscovery.buildIndicesWithSeed(numTimeSliceIndicesToTest.max, seed, bloomFilterSize, indexParameter)
-            for (nThreads <- numThreadss) {
-              logger.debug(s" Running nThreads $nThreads")
-              val resultDirPrefix = s"${bloomFilterSize}_${seed}_${epsilonFactor}_${deltaFactor}_${nThreads}_$inputSizeFactor"
-              relaxedShiftedTemporalINDDiscovery.nThreads = nThreads
-              ParallelExecutionHandler.initContext(nThreads)
-              queryFiles.foreach(queryFile => {
-                logger.debug(s"Processing queryFile $queryFile")
-                val resultSerializer = new StandardResultSerializer(new File(targetRootDir), new File(queryFile), timeSliceChoiceMethod, Some(resultDirPrefix))
-                relaxedShiftedTemporalINDDiscovery.tINDSearch(new File(queryFile), numTimeSliceIndicesToTest, queryParameters, resultSerializer)
-              })
-            }
-          }
-        })
-      })
+      for(relativeEpsilon <- relativeEpsilons){
+        logger.debug(s"Processing relative Epsilon $relativeEpsilon")
+        for(maxDeltaWhileIndexing <- maxDeltasWhileIndexing){
+          logger.debug(s"Processing max Delta $maxDeltaWhileIndexing")
+          indexEpsilonFactors.foreach(epsilonFactor => {
+            logger.debug(s"Processing epsilonFactor $epsilonFactor")
+            indexDeltaFactors.foreach(deltaFactor => {
+              logger.debug(s"Processing deltaFactor $deltaFactor")
+              val absoluteExpectedEpsilon = relativeEpsilon * GLOBAL_CONFIG.totalTimeInNanos
+              val expectedOmega = new ConstantWeightFunction()
+              val maxDeltaInNanos = TimeUtil.nanosPerDay * maxDeltaWhileIndexing
+              val queryParameters = TINDParameters(absoluteExpectedEpsilon, maxDeltaInNanos, expectedOmega)
+              val indexDelta = maxDeltaInNanos * deltaFactor
+              val indexEpsilon = absoluteExpectedEpsilon * epsilonFactor
+              val indexParameter = TINDParameters(indexEpsilon, indexDelta, expectedOmega)
+              for (inputSizeFactor <- inputSizeFactors) {
+                logger.debug(s" Running inputSizeFactor $inputSizeFactor")
+                relaxedShiftedTemporalINDDiscovery.useSubsetOfData(inputSizeFactor)
+                relaxedShiftedTemporalINDDiscovery.buildIndicesWithSeed(numTimeSliceIndicesToTest.max, seed, bloomFilterSize, indexParameter)
+                for (nThreads <- numThreadss) {
+                  logger.debug(s" Running nThreads $nThreads")
+                  val resultDirPrefix = s"${bloomFilterSize}_${seed}_${epsilonFactor}_${deltaFactor}_${nThreads}_${inputSizeFactor}_${relativeEpsilon}_$maxDeltaWhileIndexing"
+                  relaxedShiftedTemporalINDDiscovery.nThreads = nThreads
+                  ParallelExecutionHandler.initContext(nThreads)
+                  queryFiles.foreach(queryFile => {
+                    logger.debug(s"Processing queryFile $queryFile")
+                    val resultSerializer = new StandardResultSerializer(new File(targetRootDir), new File(queryFile), timeSliceChoiceMethod, Some(resultDirPrefix))
+                    relaxedShiftedTemporalINDDiscovery.tINDSearch(new File(queryFile), numTimeSliceIndicesToTest, queryParameters, resultSerializer)
+                  })
+                }
+              }
+            })
+          })
+        }
+      }
     }
   }
-
-
-
   ParallelExecutionHandler.service.shutdown()
 
 }
